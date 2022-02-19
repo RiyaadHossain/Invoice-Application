c8501cabab538b5f0efa38a48812754dda3115d1
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-19 20:12:07 +0600

    JS basic task added

---
+++ JS/app.js
@@ -0,0 +1,20 @@
+/* =============== Get All Global Variables =============== */
+// Input 
+const buyer_Details = document.getElementById("buyer-details-input");
+const item_Name = document.getElementById("item-name-input")
+const item_Price = document.getElementById("item-price-input")
+const item_Quantity = document.getElementById("item-quantity-input")
+
+// Display
+const date = document.getElementById("show-date")
+const buyer_Info = document.getElementById("buyer-info")
+const table_Body = document.getElementById("info-table")
+
+
+/* =============== Submit Button =============== */
+document.getElementById("detail-btn").addEventListener("click", () => {
+
+});
+
+/* =============== Add Button =============== */
+document.getElementById("add-btn").addEventListener("click", () => {});
